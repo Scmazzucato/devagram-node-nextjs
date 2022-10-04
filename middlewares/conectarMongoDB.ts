b4c2a0fa0564b5365ae1e827d305df4d990d47d3
Samuel Mazzucato <scmazzucato@gmail.com>
2022-10-04 20:51:10 -0300

    configuração do banco de dados

---
+++ middlewares/conectarMongoDB.ts
@@ -0,0 +1,29 @@
+import type {NextApiHandler, NextApiRequest, NextApiResponse} from 'next';
+import mongoose from 'mongoose';
+import {respostaPadraoMsg} from '../types/respostaPadraoMsg'
+
+export const conectarMongoDB = (handler : NextApiHandler) => async (
+    req : NextApiRequest,
+    res : NextApiResponse<respostaPadraoMsg>
+) =>{
+    // verificar se o banco já está conectado, se sim seguir para o endpoint
+    if(mongoose.connections[0].readyState){
+        return handler(req,res);
+    };
+
+    // já que não está conectado vamos conectar
+    // obter a variavel de ambiente preenchida do env
+    const {DB_CONEXAO_STRING} = process.env; 
+
+    // se a env estiver vazia abortar o uso do sistema e avisa o programador 
+    if(!DB_CONEXAO_STRING){
+        return res.status(500).json({error : 'Env de configuração do banco não informado'});
+    };
+
+    mongoose.connection.on('connected', () => console.log('Banco de dados conectado'));
+    mongoose.connection.on('error', error => console.log(`Ocorreu erro ao conectar no banco: ${error}`));
+    await mongoose.connect(DB_CONEXAO_STRING);
+
+    // agora posso seguir para o endpoint, pois estou conectado no banco
+    return handler(req, res);
+}
--- pages/api/login.ts
+++ pages/api/login.ts
@@ -1,8 +1,10 @@
 import type {NextApiRequest, NextApiResponse} from 'next';
+import {conectarMongoDB} from '../../middlewares/conectarMongoDB';
+import {respostaPadraoMsg} from '../../types/respostaPadraoMsg'
 
-export default (
+const endpointLogin = (
     req : NextApiRequest, 
-    res : NextApiResponse
+    res : NextApiResponse<respostaPadraoMsg>
 ) => {
     if(req.method == 'POST'){
         const {login, senha} = req.body;
@@ -11,7 +13,9 @@ export default (
         senha ==='admin@123'){
             return res.status(200).json({msg : 'Usuário autenticado com sucesso'})
         }
-        return res.status(400).json({erro : 'Usuário ou senha não encontrado'});
+        return res.status(400).json({error : 'Usuário ou senha não encontrado'});
     }
-    return res.status(405).json({erro : 'Método informado inválido'});
+    return res.status(405).json({error : 'Método informado inválido'});
 }
+
+export default conectarMongoDB(endpointLogin);
